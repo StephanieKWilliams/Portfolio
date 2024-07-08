572f5a76b7d4e04c3c3d8d921576e4b6f1b57d60
StephanieKWilliams <beefouryoux@gmail.com>
2024-07-08 16:16:25 +0300

    fix/routes to different pages , about section content

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,23 +4,25 @@ import React from 'react'
 function about() {
   return (
   <div>
+    <section id='about'>
+    <h1 className="card-title text-center font-bold flex justify-center mt-8 text-4xl mb-4 md:text-center">Get to know Me 😉</h1>
      <div className="card lg:card-side bg-base-100 shadow-xl p-4 m-2">
         <figure className="rounded-lg overflow-hidden">
           <img
             src="https://res.cloudinary.com/dsnzpqfvd/image/upload/v1720428105/WhatsApp_Image_2024-07-08_at_11.32.25_yjpivb.jpg"
             alt="Album"
-            className="w-full h-auto rounded-lg" />
+            className="w-full h-auto object-cover rounded-lg"  /> 
         </figure>
         <div className="card-body p-4">
-          <h2 className="card-title text-center font-bold text-xl mb-2 md:text-center">About Me 😉</h2>
+         
           <p className="text-gray-700 md:text-center">Something yada yada yada.</p>
           <div className="card-actions justify-end mt-4">
             <button className="btn btn-primary">Click Me</button>
-    </div>
-  </div>
-</div>
+          </div>
+     </div>
+      </div>
 
-      
+      </section> 
     </div>
   )
 }
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
+import About from './about'
+import Footer from './footer'
+
 
 
 function hero() {
   return (
     <div>
-      <div
-  className="hero min-h-screen rounded-lg overflow-hidden bg-cover bg-center"
-  style={{
-    backgroundImage: "url(https://res.cloudinary.com/dsnzpqfvd/image/upload/v1720428105/WhatsApp_Image_2024-07-08_at_11.32.25_yjpivb.jpg)",
-  }}>
-  <div className="hero-overlay bg-opacity-60"></div>
-  <div className="hero-content text-neutral-content text-center">
-    <div className="max-w-md mx-auto px-4">
-      <h1 className="mb-5 text-5xl font-bold">Grow your Page</h1>
-      <p className="mb-5">
-        Let's Gooo!!
-      </p>
-      <button className="btn btn-primary">Click Me</button>
-    </div>
-  </div>
-</div>
+      <section id='hero'>
+          <div
+            className="hero min-h-screen rounded-lg overflow-hidden bg-cover bg-center"
+            style={{
+              backgroundImage: "url(https://res.cloudinary.com/dsnzpqfvd/image/upload/v1720428105/WhatsApp_Image_2024-07-08_at_11.32.25_yjpivb.jpg)",
+            }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-neutral-content text-center">
+              <div className="max-w-md mx-auto px-4">
+                <h1 className="mb-5 text-5xl font-bold">Grow your Page</h1>
+                <p className="mb-5">
+                  Let's Gooo!!
+                </p>
+                <button className="btn btn-primary">Click Me</button>
+              </div>
+            </div>
+          </div>
+       </section> 
+       <About/>
+       <Footer/>
     </div>
   )
 }
