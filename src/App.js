67548fc6d55d474d827e70798269f22d4d41bcec
StephanieKWilliams <beefouryoux@gmail.com>
2024-07-08 12:35:29 +0300

    feature/added loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useState , useEffect} from "react";
 import Navbar from "./components/Navbar.jsx"
 import Carousel from "./components/carousel.jsx"
 import About from "./components/about.jsx";
 import Experience from "./components/experience.jsx"
 import Footer from "./components/footer.jsx"
 import Hero from "./components/hero.jsx"
+import Loading from "./components/loader.jsx";
 
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate data fetching
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000); // Change 3000 to the actual time it takes to fetch your data
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <div className="App">
       <Navbar/>
---
+++ src/components/loader.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function loader() {
+  return (
+    <div>
+        <div className="flex justify-center items-center h-screen">
+            <span className="loading loading-spinner text-primary"></span>
+            <span className="loading loading-spinner text-secondary"></span>
+            <span className="loading loading-spinner text-accent"></span>
+            <span className="loading loading-spinner text-neutral"></span>
+            <span className="loading loading-spinner text-info"></span>
+            <span className="loading loading-spinner text-success"></span>
+            <span className="loading loading-spinner text-warning"></span>
+            <span className="loading loading-spinner text-error"></span>
+        </div>
+
+    </div>
+  )
+}
+
+export default loader
