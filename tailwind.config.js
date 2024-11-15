d688385db27d4e1e53506642c8b1a06bceb46ba2
StephanieKWilliams <beefouryoux@gmail.com>
2024-11-15 15:55:30 +0300

    font change to monteserrat

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans:['Montserrat', 'sans-serif'],
+      }
+        },
     daisyui: {
       themes: ["light", "dark"],
     },
