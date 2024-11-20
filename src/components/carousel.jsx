4983fd08736ecb999dc434a6b478eaf1d4d35f8f
StephanieKWilliams <beefouryoux@gmail.com>
2024-11-20 13:33:00 +0300

    "optimized services for mobile and desktop"

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -2,43 +2,11 @@ import React from 'react';
 import Cards from './cards.jsx';
 
 function Carousel() {
-
   return (
-    
-    <div className="flex flex-wrap justify-center items-stretch">
+    <div className="flex justify-center items-center py-4">
       {/* Grid container */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 w-full">
-     
-         {/* Carousel items */}
-         <div className="carousel-item">
-          <Cards 
-            imgSrc="https://res.cloudinary.com/dsnzpqfvd/image/upload/v1720015108/WhatsApp_Image_2024-07-03_at_15.45.07_dqmzy3.jpg" 
-            altText="Image 1 Description" 
-            title="Card Title 1" 
-            description="This is the first card." 
-            buttonText="Action 1" 
-          />
-        </div>
-
-        <div className="carousel-item">
-          <Cards 
-            imgSrc="https://res.cloudinary.com/dsnzpqfvd/image/upload/v1720428105/WhatsApp_Image_2024-07-08_at_11.32.25_yjpivb.jpg" 
-            altText="Image 1 Description" 
-            title="Card Title 2" 
-            description="This is the first card." 
-            buttonText="Action 1" 
-          />
-        </div>
-        
-        <div className="carousel-item">
-          <Cards 
-            imgSrc="https://res.cloudinary.com/dsnzpqfvd/image/upload/v1720691715/WhatsApp_Image_2024-07-11_at_12.51.28_1_dld37r.jpg" 
-            altText="Image 1 Description" 
-            title="Card Title 3" 
-            description="This is the first card." 
-            buttonText="Action 1" 
-          />
-        </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full max-w-screen-xl px-4">
+        {/* Carousel items */}
         <div className="carousel-item">
           <Cards 
             imgSrc="https://res.cloudinary.com/dsnzpqfvd/image/upload/v1720015108/WhatsApp_Image_2024-07-03_at_15.45.07_dqmzy3.jpg" 
@@ -48,32 +16,53 @@ function Carousel() {
             buttonText="Action 1" 
           />
         </div>
-
         <div className="carousel-item">
           <Cards 
             imgSrc="https://res.cloudinary.com/dsnzpqfvd/image/upload/v1720428105/WhatsApp_Image_2024-07-08_at_11.32.25_yjpivb.jpg" 
-            altText="Image 1 Description" 
+            altText="Image 2 Description" 
             title="Card Title 2" 
-            description="This is the first card." 
-            buttonText="Action 1" 
+            description="This is the second card." 
+            buttonText="Action 2" 
           />
         </div>
-        
         <div className="carousel-item">
           <Cards 
-            imgSrc="https://res.cloudinary.com/dsnzpqfvd/image/upload/v1720691715/WhatsApp_Image_2024-07-11_at_12.51.28_1_dld37r.jpg" 
-            altText="Image 1 Description" 
+            imgSrc="https://res.cloudinary.com/dsnzpqfvd/image/upload/v1720428105/WhatsApp_Image_2024-07-08_at_11.32.25_yjpivb.jpg" 
+            altText="Image 3 Description" 
             title="Card Title 3" 
-            description="This is the first card." 
-            buttonText="Action 1" 
+            description="This is the third card." 
+            buttonText="Action 3" 
           />
         </div>
-
-     
-        {/* More carousel items */}
+        <div className="carousel-item">
+          <Cards 
+            imgSrc="https://res.cloudinary.com/dsnzpqfvd/image/upload/v1720428105/WhatsApp_Image_2024-07-08_at_11.32.25_yjpivb.jpg" 
+            altText="Image 4 Description" 
+            title="Card Title 4" 
+            description="This is the fourth card." 
+            buttonText="Action 4" 
+          />
         </div>
-     </div>
-   
+        <div className="carousel-item">
+          <Cards 
+            imgSrc="https://res.cloudinary.com/dsnzpqfvd/image/upload/v1720428105/WhatsApp_Image_2024-07-08_at_11.32.25_yjpivb.jpg" 
+            altText="Image 5 Description" 
+            title="Card Title 5" 
+            description="This is the fifth card." 
+            buttonText="Action 5" 
+          />
+        </div>
+        <div className="carousel-item">
+          <Cards 
+            imgSrc="https://res.cloudinary.com/dsnzpqfvd/image/upload/v1720428105/WhatsApp_Image_2024-07-08_at_11.32.25_yjpivb.jpg" 
+            altText="Image 6 Description" 
+            title="Card Title 6" 
+            description="This is the sixth card." 
+            buttonText="Action 6" 
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
